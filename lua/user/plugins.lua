9d095b60127ffc4bee0aec26ba2b4e82d0ede8a2
han feng <csfenghan@outlook.com>
2023-03-08 13:33:55 +0800

    ready to support lsp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,4 +3,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  -- Lsp support
+  use {'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim'}
 end)
