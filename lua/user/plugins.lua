709c4d99cc981664b2744ca7584481acbca785e7
han.feng <csfenghan@outlook.com>
2023-03-09 23:05:55 +0800

    complete lsp config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -5,10 +5,23 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- Lsp support
-  use {'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim'}
+  use {
+    'williamboman/mason.nvim', 
+    'williamboman/mason-lspconfig.nvim'
+  }
 
   -- Completion engine
-  use {'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline', 'hrsh7th/nvim-cmp'}
-  -- use {'hrsh7th/cmp-vsnip', 'hrsh7th/vim-vsnip'}
-  use {'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip'}
+  use {
+    'neovim/nvim-lspconfig', 
+    'hrsh7th/cmp-nvim-lsp', 
+    'hrsh7th/cmp-buffer', 
+    'hrsh7th/cmp-path', 
+    'hrsh7th/cmp-cmdline', 
+    'hrsh7th/nvim-cmp'
+  }
+
+  use {
+    'hrsh7th/cmp-vsnip', 
+    'hrsh7th/vim-vsnip'
+  }
 end)
