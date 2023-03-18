4bfbaefc6024dfb8d82c6aafba5b83505ecbbecd
fenghan <csfenghan@outlook.com>
2023-03-18 22:25:56 +0800

    support cmake

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-sudo apt install npm zip ripgrep clang-format
+sudo apt install npm zip ripgrep clang-format python3-venv python3-virtualenv
+pip install virtualenv
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
