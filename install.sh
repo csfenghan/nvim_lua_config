e97c6657a801448d32ddc71d4198893dd080ee81
han feng <csfenghan@outlook.com>
2023-03-21 18:25:12 +0800

    fix low version node result to pyright not work bug

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# install Dependency
 sudo apt install npm zip ripgrep clang-format python3-venv python3-virtualenv
 pip install virtualenv
+
+# update node for pyright
+npm install -g n && n latest && hash -r
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
